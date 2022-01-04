1517f6fc5b83c227d68455be8355b50c95f9c0e9
andrei.bekh <andrei.bekh@leverx.com>
2022-01-04 17:52:12 +0300

    Created entities and related repositories

---
+++ src/main/java/com/bekh/internship/repository/EmployeeProjectRepository.java
@@ -0,0 +1,6 @@
+package com.bekh.internship.repository;
+
+import com.bekh.internship.model.EmployeeProject;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeProjectRepository extends JpaRepository<EmployeeProject, Long> {}
