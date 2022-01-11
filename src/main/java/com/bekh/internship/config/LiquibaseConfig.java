22fc1036d2c95a314e072598893d7c04e4392c6c
andrei.bekh <andrei.bekh@leverx.com>
2022-01-11 11:58:29 +0300

    Configured liquibase

---
+++ src/main/java/com/bekh/internship/config/LiquibaseConfig.java
@@ -0,0 +1,32 @@
+package com.bekh.internship.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfig {
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(dataSource());
+        return liquibase;
+    }
+
+    public DataSource dataSource(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("org.postgresql.Driver");
+        hikariConfig.setJdbcUrl("jdbc:postgresql://localhost:5432/internship?createDatabaseIfNotExist=true");
+        hikariConfig.setUsername("postgres");
+        hikariConfig.setPassword("1234");
+        return new HikariDataSource(hikariConfig);
+    }
+}
