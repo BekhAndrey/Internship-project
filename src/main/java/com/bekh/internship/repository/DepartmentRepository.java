e447899f30951e93684f7b85a91c5c0d231003be
andrei.bekh <andrei.bekh@leverx.com>
2022-01-11 12:03:16 +0300

    Added necessary queries

--- src/main/java/com/bekh/internship/repository/DepartmentRepository.java
+++ src/main/java/com/bekh/internship/repository/DepartmentRepository.java
@@ -3,4 +3,10 @@ package com.bekh.internship.repository;
 import com.bekh.internship.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DepartmentRepository extends JpaRepository<Department, Long> {}
+import java.util.Optional;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+  void deleteById(Long id);
+
+  Optional<Department> findByTitle(String title);
+}
